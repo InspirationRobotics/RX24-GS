862b89986a343cb0b4b7acadd486b43de9cce44c
ExtraExaByte <55938181+ExtraExaByte@users.noreply.github.com>
2024-10-23 22:25:20 -0700

    WIP

--- dev/run_gs.py
+++ dev/run_gs.py
@@ -1,4 +1,8 @@
 from ground_core import GroundStation
+from ground_core.light_status import FlaskApp
+import requests
+flaskApp = None
+flask_url = 'http://127.0.0.1:5000/update_colors'
 
 def system_msg_callback(msg):
     '''
@@ -7,8 +11,31 @@ def system_msg_callback(msg):
     Note that this is a blocking function so nothing with loops or sleeps should be done here.
     '''
     print("System message received: ", msg)
+    
+    
+    
+def lightTower_heartbeat_callback(self, msg):
+    """
+    This callback will be triggered for the ScanTheCode mission
+
+    """
+    
+    data = {
+        'colors': msg,
+    }
+    
+    # Send the POST request with the new colors
+    response = requests.post(flask_url, json=data)
+    
+def startFlask(self):
+    self.flaskApp = FlaskApp()
+    self.flaskApp.run()
+    
+    
+    
 
 
 if __name__ == "__main__":
     gs = GroundStation()
     gs.add_callback("RXPTH", system_msg_callback)
+    gs.add_callback("RXCOD", lightTower_heartbeat_callback)
